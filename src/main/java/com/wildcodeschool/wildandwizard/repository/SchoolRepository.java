c09fdf7139c3c51b30898781f7651543008a8d1d
bormannb <99260828+bormannb@users.noreply.github.com>
2022-05-09 10:39:23 +0200

    Add files via upload

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,6 +1,14 @@
 package com.wildcodeschool.wildandwizard.repository;
 
+import java.util.Optional;
+import com.wildcodeschool.wildandwizard.entity.School;
+import com.wildcodeschool.wildandwizard.entity.Wizard;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 // TODO : update this interface
 
-public interface SchoolRepository {
+public interface SchoolRepository extends JpaRepository<School, Long>{
+    
 }
